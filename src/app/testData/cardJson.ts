e846cc43c9a8207764d2bf328784d37c68030d37
Connor Robinson <connorrobinson121@gmail.com>
2021-11-01 12:49:04 -0400

    Adding 3 test cards

---
+++ src/app/testData/cardJson.ts
@@ -0,0 +1,36 @@
+export const CARDJSON: Object[] = [
+    {
+        "_id": "616b5c590d5499a33a090908",
+        "name": "Opt",
+        "cmc": 1,
+        "colors": [
+            "U"
+        ],
+        "mana_cost": "{U}",
+        "oracle_text": "Scry 1.\nDraw a card.",
+        "type_line": "Instant"
+    },
+    {
+        "_id": "616b5c170d5499a33a087f25",
+        "name": "Snapcaster Mage",
+        "cmc": 2,
+        "colors": [
+            "U"
+        ],
+        "mana_cost": "{1}{U}",
+        "oracle_text": "Flash\nWhen Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost. (You may cast that card from your graveyard for its flashback cost. Then exile it.)",
+        "type_line": "Creature — Human Wizard"
+    },
+    {
+        "_id": "616b5bfd0d5499a33a08460f",
+        "name": "Owlbear",
+        "__v": 0,
+        "cmc": 5,
+        "colors": [
+            "G"
+        ],
+        "mana_cost": "{3}{G}{G}",
+        "oracle_text": "Trample\nKeen Senses — When Owlbear enters the battlefield, draw a card.",
+        "type_line": "Creature — Bird Bear"
+    }
+]
