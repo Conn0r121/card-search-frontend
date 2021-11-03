e7a71c6618b4b890c58eb8cd0d8b91655e82600c
Connor Robinson <connorrobinson121@gmail.com>
2021-11-03 19:42:05 -0400

    Adding queryParams for card Search

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CardService } from '../services/card.service';
 import { Card } from '../classes/card';
 
@@ -10,16 +11,21 @@ import { Card } from '../classes/card';
 export class CardComponent implements OnInit {
   public card!: Card;
 
-  constructor(private _cardService: CardService) {
+  constructor(private _cardService: CardService, private _route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this._getCard("Snapcaster Mage");
+    this._route.queryParams.subscribe(params => {
+      const cardName = params.name;
+      this._getCard(cardName);
+    });
   }
 
   private _getCard(name: string): void {
     this._cardService.getSingleCard(name)
-      .subscribe(card => this.card = card);
+      .subscribe(card => {
+        this.card = card;
+      });
   }
 
 }
--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Card } from '../classes/card';
 import { CARDJSON } from '../testData/cardJson';
-import { Observable, of } from 'rxjs';
+import { Observable, of} from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
 
-  constructor() { }
+  private _API_KEY = "https://rbrmaqc4bg.execute-api.us-east-2.amazonaws.com/alpha/"
+
+  constructor(private _http: HttpClient) { }
 
   public getSingleCard(name: string): Observable<Card> {
-    return of(Card.fromJson(CARDJSON.find(card => card.name === name)));
+    // return of(Card.fromJson(CARDJSON.find(card => card.name === name)));
+
+    const uri = `${this._API_KEY}/card?name=${name}`;
+
+     return this._http.get<any>(uri)
+      .pipe(map(response => Card.fromJson(response[0])));
   }
 }
