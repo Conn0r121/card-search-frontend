48d35ec12fb07e2ca04b07347ac73d96178b1e3f
Connor Robinson <connorrobinson121@gmail.com>
2021-11-03 20:43:05 -0400

    Getting Tests to work

--- src/app/card/card.component.spec.ts
+++ src/app/card/card.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CardComponent } from './card.component';
 
@@ -8,6 +11,7 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, BrowserModule, RouterTestingModule],
       declarations: [ CardComponent ]
     })
     .compileComponents();
