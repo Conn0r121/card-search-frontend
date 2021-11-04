b4afe861c98e53407a6a543d21e0cfdbfdac4835
Connor Robinson <connorrobinson121@gmail.com>
2021-11-03 23:01:33 -0400

    Adding card service tests

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,19 +1,44 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CardService } from './card.service';
+import { Card } from '../classes/card';
+import { CARDJSON } from '../testData/cardJson'
 
 describe('CardService', () => {
+  let httpTestingController: HttpTestingController;
   let service: CardService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule]
     });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(CardService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('get card', () => {
+    const cardJson = CARDJSON[0];
+    const expectedCardObject = Card.fromJson(cardJson);
+    service.getSingleCard('Opt').subscribe(returnedCardObject => {
+      expect(returnedCardObject).toEqual(expectedCardObject);
+    });
+
+    const expectedURI = 'https://rbrmaqc4bg.execute-api.us-east-2.amazonaws.com/alpha//card?name=Opt';
+
+    const req = httpTestingController.expectOne(expectedURI);
+
+    expect(req).toBeTruthy();
+
+    req.flush([cardJson]);
+  });
+
 });
